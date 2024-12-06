adb49f483e9c289f9fc7adf99aa177f10e2fccec
Stefan Buenaventura <stefanbuenaventura@gmail.com>
2024-12-06 21:30:16 +0800

    feat: improve email and password validations

--- src/app/(auth)/validations.ts
+++ src/app/(auth)/validations.ts
@@ -2,19 +2,32 @@ import { z } from "zod"
 
 export const email = z
   .string()
-  .email()
+  .min(1, { message: "Email is required" })
+  .email({ message: "Must be a valid Email" })
   .transform((str) => str.toLowerCase().trim())
 
 export const password = z
   .string()
-  .min(10)
-  .max(100)
+  .min(1, { message: "Password is required" })
+  .min(10, { message: "Password must contain at least 10 characters" })
+  .max(100, { message: "Password must contain less than 100 characters" })
   .transform((str) => str.trim())
 
-export const Signup = z.object({
-  email,
-  password,
-})
+export const Signup = z
+  .object({
+    email,
+    password,
+    confirmPassword: password,
+  })
+  .superRefine(({ confirmPassword, password }, ctx) => {
+    if (confirmPassword !== password) {
+      ctx.addIssue({
+        code: "custom",
+        message: "The passwords did not match",
+        path: ["confirmPassword"],
+      })
+    }
+  })
 
 export const Login = z.object({
   email,
